1e7b44d82f3674bbfe63bd4154ad3d754ede32c6
LCDZhao-O <weizhao@oppo.com>
2021-06-29 14:23:25 +0800

    refactor(CompositeRequest): Replace the duplicate code in compositeRequest.segment() with the HttpRequestBaseImpl.copyTo() method

--- httpclient-core/src/main/java/esa/httpclient/core/CompositeRequest.java
+++ httpclient-core/src/main/java/esa/httpclient/core/CompositeRequest.java
@@ -115,20 +115,7 @@ public class CompositeRequest extends HttpRequestBaseImpl implements PlainReques
     public SegmentRequest segment() {
         checkNotStartedAndUpdateStatus(STATE_SEGMENT_PREPARING);
         SegmentRequest segmentRequest = request.get();
-        for (String name : paramNames()) {
-            for (String value : getParams(name)) {
-                segmentRequest.addParam(name, value);
-            }
-        }
-        headers().forEach((header) -> segmentRequest.addHeader(header.getKey(), header.getValue()));
-        segmentRequest.readTimeout(readTimeout());
-        if (uriEncode()) {
-            segmentRequest.enableUriEncode();
-        }
-        segmentRequest.maxRedirects(ctx.maxRedirects);
-        segmentRequest.maxRetries(ctx.maxRetries);
-        segmentRequest.handle(handle);
-        segmentRequest.handler(handler);
+        HttpRequestBaseImpl.copyTo(this, (HttpRequestBaseImpl) segmentRequest);
         return segmentRequest;
     }
 

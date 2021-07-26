7e44931f6858da1f6f474dc09808eec5d104952f
LCDZhao-O <weizhao@oppo.com>
2021-07-26 17:57:47 +0800

    add necessary comments to decoder and encoder

--- restclient/src/main/java/esa/restclient/codec/Decoder.java
+++ restclient/src/main/java/esa/restclient/codec/Decoder.java
@@ -7,8 +7,29 @@ import esa.restclient.ResponseBodyContent;
 
 import java.lang.reflect.Type;
 
+/**
+ * <code>Decoder</code> is designed for the conversion of a {@link ResponseBodyContent} to a Java type.And it
+ * is dynamically bound to the response through the {@link DecoderSelector}.
+ * <p>
+ * If you want to implement a <code>Decoder</code>, please implement the {@link ByteDecoder} interface inherited from the
+ * <code>Decoder</code> instead of directly using the internal interface of the <code>Decoder</code>
+ *
+ * @see ResponseBodyContent
+ * @see ByteDecoder
+ */
 @Internal
 public interface Decoder {
 
+    /**
+     * Decode the {@link ResponseBodyContent} into a Java object
+     *
+     * @param mediaType the media type of the HTTP response
+     * @param headers   the headers of the HTTP response
+     * @param content   the {@link ResponseBodyContent} of the HTTP response
+     * @param type      the type that is to be decode from the content.
+     * @param <T>       generic type
+     * @return decoded value
+     * @throws Exception error
+     */
     <T> T decode(MediaType mediaType, HttpHeaders headers, ResponseBodyContent<?> content, Type type) throws Exception;
 }
--- restclient/src/main/java/esa/restclient/codec/DecoderSelector.java
+++ restclient/src/main/java/esa/restclient/codec/DecoderSelector.java
@@ -1,5 +1,6 @@
 package esa.restclient.codec;
 
+import esa.commons.annotation.Internal;
 import esa.commons.http.HttpHeaders;
 import esa.httpclient.core.util.Ordered;
 import esa.restclient.ContentType;
@@ -9,6 +10,16 @@ import esa.restclient.RestRequest;
 
 import java.lang.reflect.Type;
 
+/**
+ * The <code>DecoderSelector</code> is designed to dynamically find the appropriate {@link Decoder} for the response.
+ * <p>
+ * If you want to implement a <code>DecoderSelector</code>, please implement the {@link ByteDecoderSelector} interface inherited from the
+ * <code>DecoderSelector</code> instead of directly using the internal interface of the <code>DecoderSelector</code>
+ *
+ * @see Decoder
+ * @see ByteDecoderSelector
+ */
+@Internal
 public interface DecoderSelector extends Ordered {
     Decoder select(RestRequest request, ContentType[] acceptTypes, MediaType responseMediaType,
                    HttpHeaders responseHeaders, ResponseBodyContent<?> data, Type type);

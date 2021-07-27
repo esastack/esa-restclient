a4785751ea84fb915e78f3282ab1f3735c62186e
LCDZhao-O <weizhao@oppo.com>
2021-07-27 19:22:03 +0800

    modify the comments of decode and encode related classes

--- restclient/src/main/java/esa/restclient/codec/DecoderSelector.java
+++ restclient/src/main/java/esa/restclient/codec/DecoderSelector.java
@@ -11,17 +11,30 @@ import esa.restclient.RestRequest;
 import java.lang.reflect.Type;
 
 /**
- * The <code>DecoderSelector</code> is designed to dynamically find the appropriate {@link Decoder} for the response.
+ * The <code>DecoderSelector</code> is designed to dynamically find the appropriate {@link Decoder}
+ * for the response.
  * <p>
- * If you want to implement a <code>DecoderSelector</code>, please implement the {@link ByteDecoderSelector}
- * interface inherited from the <code>DecoderSelector</code> instead of directly using the internal interface of
- * the <code>DecoderSelector</code>
+ * If you want to implement a <code>DecoderSelector</code>, please implement the
+ * {@link ByteDecoderSelector} interface inherited from the <code>DecoderSelector</code> instead of
+ * directly using the internal interface of the <code>DecoderSelector</code>
  *
  * @see Decoder
  * @see ByteDecoderSelector
  */
 @Internal
 public interface DecoderSelector extends Ordered {
+
+    /**
+     * select a appropriate decoder to decode response data
+     *
+     * @param request           request
+     * @param acceptTypes       acceptTypes of request
+     * @param responseMediaType responseMediaType
+     * @param responseHeaders   responseHeaders
+     * @param data              data
+     * @param type              type
+     * @return decoder
+     */
     Decoder select(RestRequest request, ContentType[] acceptTypes, MediaType responseMediaType,
                    HttpHeaders responseHeaders, ResponseBodyContent<?> data, Type type);
 

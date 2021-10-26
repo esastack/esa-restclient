0fb6bea6b04dd8aab41737310e9e0da134af087f
LCDZhao-O <weizhao@oppo.com>
2021-10-26 15:06:07 +0800

    complete FileToFileEncoderTest

--- restclient/src/main/java/io/esastack/restclient/codec/impl/FileToFileEncoder.java
+++ restclient/src/main/java/io/esastack/restclient/codec/impl/FileToFileEncoder.java
@@ -12,7 +12,7 @@ public class FileToFileEncoder implements Encoder {
     @Override
     public RequestBodyContent<File> encode(MediaType mediaType, HttpHeaders headers, Object entity) {
         if (entity == null) {
-            return null;
+            return RequestBodyContent.of((File) null);
         }
 
         return RequestBodyContent.of((File) entity);
---
+++ restclient/src/test/java/io/esastack/restclient/exec/FileToFileEncoderTest.java
@@ -0,0 +1,23 @@
+package io.esastack.restclient.exec;
+
+import io.esastack.restclient.codec.impl.FileToFileEncoder;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class FileToFileEncoderTest {
+
+    @Test
+    void testEncode() {
+        FileToFileEncoder fileEncoder = new FileToFileEncoder();
+        then(fileEncoder.encode(null, null, null).content())
+                .isEqualTo(null);
+
+        File file = new File("test");
+        then(fileEncoder.encode(null, null, file).content())
+                .isEqualTo(file);
+    }
+
+}

210364ad41babdc6bfa1a831a3d245a20ce53c54
LCDZhao-O <weizhao@oppo.com>
2021-10-26 16:02:12 +0800

    complete StringCodecTest

---
+++ restclient/src/test/java/io/esastack/restclient/exec/StringCodecTest.java
@@ -0,0 +1,49 @@
+package io.esastack.restclient.exec;
+
+import io.esastack.commons.net.http.MediaTypeUtil;
+import io.esastack.restclient.ResponseBodyContent;
+import io.esastack.restclient.codec.impl.StringCodec;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+class StringCodecTest {
+
+    @Test
+    void testEncode() throws Exception {
+
+        StringCodec codec = new StringCodec();
+
+        then(codec.encode(MediaTypeUtil.of("text", "plain", StandardCharsets.UTF_16),
+                null, null).content()).isEqualTo(null);
+
+        String content = "content";
+        byte[] bytes = content.getBytes(StandardCharsets.UTF_16);
+        then(codec.encode(MediaTypeUtil.of("text", "plain", StandardCharsets.UTF_16),
+                null, content).content()).isEqualTo(bytes);
+
+        bytes = content.getBytes(StandardCharsets.UTF_8);
+        then(codec.encode(MediaTypeUtil.of("text", "plain"),
+                null, content).content()).isEqualTo(bytes);
+
+    }
+
+    @Test
+    void testDecode() throws Exception {
+        StringCodec codec = new StringCodec();
+
+        then((String) codec.decode(MediaTypeUtil.of("text", "plain", StandardCharsets.UTF_16),
+                null, ResponseBodyContent.of(null), String.class)).isEqualTo(null);
+
+        String content = "content";
+        byte[] bytes = content.getBytes(StandardCharsets.UTF_16);
+        then((String) codec.decode(MediaTypeUtil.of("text", "plain", StandardCharsets.UTF_16),
+                null, ResponseBodyContent.of(bytes), String.class)).isEqualTo(content);
+
+        bytes = content.getBytes(StandardCharsets.UTF_8);
+        then((String) codec.decode(MediaTypeUtil.of("text", "plain"),
+                null, ResponseBodyContent.of(bytes), String.class)).isEqualTo(content);
+    }
+}

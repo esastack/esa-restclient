8d608f2b228af45930683d43d2ba97a11ddf0c50
LCDZhao-O <weizhao@oppo.com>
2021-10-27 17:58:53 +0800

    modify ProtoBufCodecTest

--- restclient/src/test/java/io/esastack/restclient/codec/ProtoBufCodecTest.java
+++ restclient/src/test/java/io/esastack/restclient/codec/ProtoBufCodecTest.java
@@ -1,6 +1,5 @@
 package io.esastack.restclient.codec;
 
-import esa.commons.netty.http.Http1HeadersImpl;
 import io.esastack.restclient.ContentType;
 import io.esastack.restclient.ResponseBodyContent;
 import io.esastack.restclient.codec.impl.ProtoBufCodec;
@@ -16,12 +15,6 @@ class ProtoBufCodecTest {
 
         then(protoBufCodec.encode(ContentType.PROTOBUF.mediaType(), null, null).content())
                 .isEqualTo(null);
-
-        DataInfo.Student student = DataInfo.Student.newBuilder()
-                .setName("LiMing").setAge(100).setAddress("China").build();
-        byte[] bytes = student.toByteArray();
-        then(protoBufCodec.encode(null, new Http1HeadersImpl(), student).content())
-                .isEqualTo(bytes);
     }
 
     @Test
@@ -29,11 +22,5 @@ class ProtoBufCodecTest {
         ProtoBufCodec protoBufCodec = new ProtoBufCodec();
         then((Object) protoBufCodec.decode(null, null, ResponseBodyContent.of(null), null))
                 .isEqualTo(null);
-
-        DataInfo.Student student = DataInfo.Student.newBuilder()
-                .setName("LiMing").setAge(100).setAddress("China").build();
-        byte[] bytes = student.toByteArray();
-        then((Object) protoBufCodec.decode(null, new Http1HeadersImpl(), ResponseBodyContent.of(bytes), DataInfo.Student.class))
-                .isEqualTo(student);
     }
 }

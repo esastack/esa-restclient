11584b2ad0a1115dc134510c8897aaeda62d74a6
LCDZhao-O <weizhao@oppo.com>
2022-04-01 16:37:59 +0800

    add MatchResultImpl

--- restclient-ext/request-redefine-interceptor/src/main/java/io/esastack/restclient/ext/condition/MatchResult.java
+++ restclient-ext/request-redefine-interceptor/src/main/java/io/esastack/restclient/ext/condition/MatchResult.java
@@ -5,4 +5,34 @@ public interface MatchResult {
     boolean isMatch();
 
     String unMatchReason();
+
+    static MatchResult success() {
+        return MatchResultImpl.SUCCESS_MATCH_RESULT;
+    }
+
+    static MatchResult fail(String reason) {
+        return new MatchResultImpl(false, reason);
+    }
+
+    class MatchResultImpl implements MatchResult {
+
+        private final boolean isMatch;
+        private final String unMatchReason;
+        private static final MatchResult SUCCESS_MATCH_RESULT = new MatchResultImpl(true, null);
+
+        private MatchResultImpl(boolean isMatch, String unMatchReason) {
+            this.isMatch = isMatch;
+            this.unMatchReason = unMatchReason;
+        }
+
+        @Override
+        public boolean isMatch() {
+            return isMatch;
+        }
+
+        @Override
+        public String unMatchReason() {
+            return unMatchReason;
+        }
+    }
 }
